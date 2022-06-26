fadac4214dc434081b73cdbb0c05466503921da7
wooods-marshes <13080352723@163.com>
2022-06-26 16:46:30 +0800

    nothing

--- app/src/main/java/io/github/woodsmarshes/toulecode/MainActivity.kt
+++ app/src/main/java/io/github/woodsmarshes/toulecode/MainActivity.kt
@@ -2,10 +2,18 @@ package io.github.woodsmarshes.toulecode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.woodsmarshes.toulecode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_black_24dp)
+        }
     }
 }
